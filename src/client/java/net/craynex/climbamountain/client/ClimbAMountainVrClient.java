a9a5d40f8befd629d0452d1bd729fc6c95ffe810
craynex <cris2007.martin@gmail.com>
2026-06-25 14:27:46 +0200

    First commit

---
+++ src/client/java/net/craynex/climbamountain/client/ClimbAMountainVrClient.java
@@ -0,0 +1,10 @@
+package net.craynex.climbamountain.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClimbAMountainVrClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/craynex/climbamountain/client/ClimbAMountainVrDataGenerator.java
@@ -0,0 +1,11 @@
+package net.craynex.climbamountain.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClimbAMountainVrDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
